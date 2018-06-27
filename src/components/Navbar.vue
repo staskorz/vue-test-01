dacd3fcf20f2547918b510b73eccd5f4486cf914
staskorz <staskorz@gmail.com>
2018-06-27 23:10:34 +0300

    Rebuilt navigation with Bootstrap

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="navbar">
+    <div class="nav justify-content-start">
+      <router-link to="/" class="navbar-brand">PETSITTER</router-link>
+    </div>
+    <div class="nav justify-content-end">
+      <router-link to="/" class="nav-item nav-link">Pet List</router-link>
+      <router-link to="/add-pet" class="nav-item nav-link">Add Pet</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+}
+</script>
