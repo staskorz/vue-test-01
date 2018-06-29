a2c1bc11086a0fc9bdf8853f71af41c41bf5e19a
staskorz <staskorz@gmail.com>
2018-06-29 18:58:51 +0300

    Added validation rule for relative URLs

--- src/form-validation/index.js
+++ src/form-validation/index.js
@@ -1,4 +1,6 @@
 import Vue from "vue"
 import VeeValidate from "vee-validate"
 
+import "./url-allow-relative"
+
 Vue.use(VeeValidate)
---
+++ src/form-validation/url-allow-relative.js
@@ -0,0 +1,11 @@
+import { Validator } from "vee-validate"
+import isURL from "validator/lib/isURL"
+
+const validatorOptions = {
+  allow_protocol_relative_urls: true,
+}
+
+Validator.extend("url_allow_relative", {
+  getMessage: field => `The ${field} is not a valid URL`,
+  validate: value => isURL(value, validatorOptions),
+})
